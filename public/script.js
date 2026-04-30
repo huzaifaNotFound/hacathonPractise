e2b6b104d6897e81a4dfbd01aba6ea911bf35b24
huzaifa <idkmyname9009@gmail.com>
2026-04-30 18:44:42 +0530

    html, css

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } from "firebase/firestore"
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
