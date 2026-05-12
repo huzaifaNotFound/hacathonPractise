43d8c6f9a1ff80cd4cfcc2f57abb886150a577ae
huzaifa <idkmyname9009@gmail.com>
2026-05-12 21:23:05 +0530

    b

--- public/script.js
+++ public/script.js
@@ -1,18 +1,45 @@
 // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-  import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } from "firebase/firestore"
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyA6r1YkNOVoygxOfycnXZIMkNEmDljfgbU",
-    authDomain: "hackathonpractise.firebaseapp.com",
-    projectId: "hackathonpractise",
-    storageBucket: "hackathonpractise.firebasestorage.app",
-    messagingSenderId: "442908890208",
-    appId: "1:442908890208:web:48104cdc9ae85ad8b5df05"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6r1YkNOVoygxOfycnXZIMkNEmDljfgbU",
+  authDomain: "hackathonpractise.firebaseapp.com",
+  projectId: "hackathonpractise",
+  storageBucket: "hackathonpractise.firebasestorage.app",
+  messagingSenderId: "442908890208",
+  appId: "1:442908890208:web:48104cdc9ae85ad8b5df05",
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(app, db);
+});
+
+// const docRef = await addDoc(collection(db, "tasks"), {
+//   first: "Ada",
+//   last: "Lovelace",
+//   born: 1815,
+// });
+// console.log(docRef);
+
+const querySnapshot = await getDocs(collection(db, "tasks"));
+querySnapshot.forEach((doc) => {
+  const data = doc.data()
+  for(let key in data){
+    console.log(key, data[key])
+  }
+});
